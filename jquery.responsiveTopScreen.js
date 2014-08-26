321a2b67ff06ce2499ebcbf5fd91a475d3f466b2
UshioShugo <ushio.s@gmail.com>
2014-08-26 15:30:25 +0900

    auto culc top screen image size

--- jquery.responsiveTopScreen.js
+++ jquery.responsiveTopScreen.js
@@ -2,8 +2,95 @@
 (function(jQuery){
 	var ResponsiveTopScreen = function() {};
 	ResponsiveTopScreen.prototype = {
-		run: function(target){
-			console.log(target);
+
+		/**
+		 *  Window element.
+		 */
+		windowElement: jQuery(window),
+
+		/**
+		 * this element will be frame.
+		 */
+		targetElement: undefined,
+
+		/**
+		 * this element will be fullscreen.
+		 */
+		targetMediaElement: undefined,
+
+		/**
+		 * run
+		 * 
+		 * @params targetElement this element will be fullscreen.
+		 */
+		run: function(targetElement){
+			var _this = this;
+			
+			// Target element settings.
+			this.setTarget(targetElement);
+			
+			// At first, call resize callback function 
+			this.onWindowResize(undefined);
+
+			// Set event listener
+			this.windowElement.resize(function(event){
+				_this.onWindowResize(event);
+			});
+		},
+
+		/**
+		 * onWindowResize
+		 * 
+		 * @params event callback event.
+		 */
+		onWindowResize: function(event){
+			var windowWidth = this.windowElement.width();
+			var windowHeight = this.windowElement.height();
+			var mediaWidth = this.targetMediaElement.data('original-width');
+			var mediaHeight = this.targetMediaElement.data('original-height');
+
+			// set target element size.
+			this.targetElement.width(windowWidth);
+			this.targetElement.height(windowHeight);
+
+			// set media element size.
+			var windowRatio = windowWidth / windowHeight;
+			var mediaRatio = mediaWidth / mediaHeight;
+
+			console.log(windowRatio, mediaRatio);
+
+			if (mediaRatio > windowRatio){
+				this.targetMediaElement.width(mediaWidth * ( windowHeight / mediaHeight ));
+				this.targetMediaElement.height(windowHeight);
+			}else{
+				this.targetMediaElement.width(windowWidth);
+				this.targetMediaElement.height(mediaHeight * ( windowWidth / mediaWidth ));
+			}
+			
+		},
+
+		/**
+		 * setTarget
+		 */
+		setTarget: function(targetElement){
+			this.targetElement = targetElement;
+			targetElement.css({
+				display: "block",
+				overflow: "hidden",
+				margin: 0,
+				padding: 0,
+				position: "relative"
+			});
+
+			var targetMediaElement = targetElement.find(":first-child");
+			this.targetMediaElement = targetMediaElement;
+			targetMediaElement.css({
+				display: "block",
+				margin: 0,
+				padding: 0,
+				position: "absolute",
+				zIndex: -10
+			});
 		}
 	};
 	
@@ -14,13 +101,13 @@
 		var options = jQuery.extend(jQuery.fn.responsiveTopScreen.defaults, options);
 		
 		// set using objects.
-		var target = jQuery(this);
+		var targetElement = jQuery(this);
 		
 		// Event start
 		topScreen = new ResponsiveTopScreen();
-		topScreen.run(target);
+		topScreen.run(targetElement);
 		
-		return target;
+		return this;
 	};
 	
 	/**
